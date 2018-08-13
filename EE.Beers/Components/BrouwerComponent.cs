911b60206328be523bbeba3719d37ff9de240a8d
BlanckeKenny <kenny.blancke@icloud.com>
2018-08-13 19:23:25 +0200

    ViewComponent added

---
+++ EE.Beers/Components/BrouwerComponent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EE.Beers.Data;
+using EE.Beers.ViewModels.Components;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EE.Beers.Components
+{
+    [ViewComponent(Name = "BrouwersLijst")]
+    public class BrouwerComponent : ViewComponent
+    {
+        private BeersContext _context;
+
+        public BrouwerComponent(BeersContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var AlleBrouwers = await _context.Brouwerijen.OrderBy(b => b.Name).ToListAsync();
+            BrouwersComponentVm vm = new BrouwersComponentVm {AlleBrouwerijen = AlleBrouwers};
+            return View(vm);
+        }
+    }
+}
---
+++ EE.Beers/ViewModels/Components/BrouwersComponentVm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EE.Beers.Entities;
+
+namespace EE.Beers.ViewModels.Components
+{
+    public class BrouwersComponentVm
+    {
+        public IEnumerable<Brouwerij> AlleBrouwerijen { get; set; }
+    }
+}
